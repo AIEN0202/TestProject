d7cfb089d019147d771cbb1fc3fb4e0fb124b84b
陳重光 <taiwanpride888@123.com>
2018-05-21 16:49:54 +0800

    xxx

--- file01.py
+++ file01.py
@@ -1,17 +1,18 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("Wiilyy!")
 =======
 print("Ciao!!!!")
->>>>>>> 40cc624d444395214047c38f60b24eee87c8506e
-=======
+>>>>>>> 40cc624d444395print("Wiilyy!")214047c38f60b24eee87c8506e
+===print("Wiilyy!")====print("Wiilyy!")
 print("Hello World!!!!")
 print("Hello World!!!!2")
-print("Hello World!!!!3")
+priprint("Wiilyy!")nt("Hello World!!!!3")
 print("Hello World!!!!4")
 <<<<<<< HEAD
->>>>>>> 18d44d5905d54ee2b2b7775d2898621fc1010ff9
-=======
+>>>>>>> 18d44d59print("Wiilyy!"print("Wiilyy!"))05d54ee2b2b7775d2898621fc1010ff9
+=======print("Wiilyy!")
 print("Hello World!!!!5")
->>>>>>> 0dc9362d095687797a0989562b77014627a3ef58
+
 print("Ciao!!!!")
+print("Wiilyy!")
+print("Wiilyy!")
+print("Wiilyy!")
